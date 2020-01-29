2e1c3d997ee562174923510f42f33e57420b921a
Cherepakhin Vasili <vasi.che@gmail.com>
2020-01-29 12:21:18 +0500

    Заготовка

---
+++ src/test/java/ru/perm/v/rent/RentApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.perm.v.rent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
